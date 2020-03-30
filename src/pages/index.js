905ff1edd2e4fe4706a2f43f104aec533b2d037e
Katherine Coleman <katherinecoleman@Katherines-MacBook-Pro.local>
2020-03-30 14:32:13 -0500

    changed color palette, added Coleman Dance Company Page under landing.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,42 +37,42 @@ class HomeIndex extends React.Component {
             <article style={{ backgroundImage: `url(${pic01})` }}>
               <header className="major">
                 <h3>Class Schedule</h3>
-                <p>Ipsum dolor sit amet</p>
+                <p>Find The Class That's Right For You</p>
               </header>
               <Link to="/landing" className="link primary"></Link>
             </article>
             <article style={{ backgroundImage: `url(${pic02})` }}>
               <header className="major">
                 <h3>Coleman Dance Company</h3>
-                <p>feugiat amet tempus</p>
+                <p>Join Our Award-Winning Student Company</p>
               </header>
               <Link to="/landing" className="link primary"></Link>
             </article>
             <article style={{ backgroundImage: `url(${pic03})` }}>
               <header className="major">
                 <h3>Faculty</h3>
-                <p>Lorem etiam nullam</p>
+                <p>Meet The Teachers</p>
               </header>
               <Link to="/landing" className="link primary"></Link>
             </article>
             <article style={{ backgroundImage: `url(${pic04})` }}>
               <header className="major">
                 <h3>Tuition and Policy</h3>
-                <p>Nisl sed aliquam</p>
+                <p>Get the Rules</p>
               </header>
               <Link to="/landing" className="link primary"></Link>
             </article>
             <article style={{ backgroundImage: `url(${pic05})` }}>
               <header className="major">
                 <h3>Summer Dance</h3>
-                <p>Ipsum dolor sit amet</p>
+                <p>Keep Dancing All Summer!</p>
               </header>
               <Link to="/landing" className="link primary"></Link>
             </article>
             <article style={{ backgroundImage: `url(${pic06})` }}>
               <header className="major">
                 <h3>Testimonials</h3>
-                <p>Feugiat amet tempus</p>
+                <p>See What Our Students Say About Us</p>
               </header>
               <Link to="/landing" className="link primary"></Link>
             </article>
@@ -80,24 +80,9 @@ class HomeIndex extends React.Component {
           <section id="two">
             <div className="inner">
               <header className="major">
-                <h2>Massa libero</h2>
+                <h2>Mission Statement</h2>
               </header>
-              <p>
-                Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-                Pellentesque condimentum sem. In efficitur ligula tate urna.
-                Maecenas laoreet massa vel lacinia pellentesque lorem ipsum
-                dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
-                sagittis libero. Mauris aliquet magna magna sed nunc rhoncus
-                amet pharetra et feugiat tempus.
-              </p>
-              <ul className="actions">
-                <li>
-                  <Link to="/landing" className="button next">
-                    Get Started
-                  </Link>
-                </li>
-              </ul>
+              <p>Mission Statement Here</p>
             </div>
           </section>
         </div>
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -8,77 +8,118 @@ import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 
-const Landing = (props) => (
-    <Layout>
-        <Helmet>
-            <title>Landing - Forty by HTML5 UP</title>
-            <meta name="description" content="Landing Page" />
-        </Helmet>
+const Landing = props => (
+  <Layout>
+    <Helmet>
+      <title>Landing - Forty by HTML5 UP</title>
+      <meta name="description" content="Landing Page" />
+    </Helmet>
 
-        <BannerLanding />
+    <BannerLanding />
 
-        <div id="main">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h2>Sed amet aliquam</h2>
-                    </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
-                </div>
-            </section>
-            <section id="two" className="spotlights">
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={pic08} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Orci maecenas</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={pic09} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Rhoncus magna</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={pic10} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Sed nunc ligula</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-            </section>
+    <div id="main">
+      <section id="one">
+        <div className="inner">
+          <header className="major">
+            <h2>Coleman Dance Company</h2>
+          </header>
+          <p>
+            The Coleman Dance Company is an award-winning student company that
+            has performed all over the east coast including Lincoln Center
+            (NYC), Lyndon Institute (VT) The Palace (CT), and is one of the few
+            companies invited to be part of the Artists For World Peace at the
+            Katherine Hepburn Cultural Arts Center. The dancers are also
+            acclaimed by the International Dance Council (IDC) and UNESCO.
+            <br />
+            <br />
+            Our company classes build rapport and support among a team of
+            collaborative and talented dancers. In each Coleman Company, dancers
+            take three classes a week, participate in multiple performances per
+            year, experience guest teachers who are experts in their fields,
+            participate in special events, and see student choreography
+            performed. When they are old enough/ready, they become mentors to a
+            designated younger dancer. Working as a team, the dancers depend on
+            each other for success, feed one another's inspirations and
+            intellect, and build long-lasting friendships.
+          </p>
         </div>
-
-    </Layout>
+      </section>
+      <section id="two" className="spotlights">
+        <section>
+          <Link to="/generic" className="image">
+            <img src={pic08} alt="" />
+          </Link>
+          <div className="content">
+            <div className="inner">
+              <header className="major">
+                <h3>Transitional Justice</h3>
+              </header>
+              <p>
+                Christina Coleman's dance piece, <i>Transitional Justice,</i> is
+                inspired by real people and events in communities around the
+                world including Columbia, Guatemala, Liberia, Sierra Leone,
+                Somalia, South Africa, West Africa, and the Balkans. The
+                performance takes the audience on a journey through several
+                different peoples' lives: Illuminating their intricate
+                relationships, betrayals, traumas, fears, desires, moments of
+                empowerment, and finally, forgiveness. The Coleman Teen Dance
+                company's performance of <i>Transitional Justice</i> won awards
+                for "Originality in Choreography" and "Excellence in Intent" at
+                the Vermont State Dance Festival in 2016. In 2017, by
+                invitation, the Coleman Company performed an excerpt of the
+                piece at Lincoln Center.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Link to="/generic" className="image">
+            <img src={pic09} alt="" />
+          </Link>
+          <div className="content">
+            <div className="inner">
+              <header className="major">
+                <h3>For Malala</h3>
+              </header>
+              <p>
+                Dancers in The Coleman Teen Dance Company fought to make a
+                difference with their piece <i>For Malala</i> choreographed by
+                student, Sophia Procario-Foley and director, Christina Coleman
+                It won awards for "Originality in Choreography" and "Excellence
+                in Intent" at The Vermont State Dance Festival, and was one of
+                few pieces to be invited to Artists For World Peace at the
+                Katherine Hepburn Cultural Arts Center in Connecticut. The
+                Coleman School of Dance hosted a dance benefit raising over
+                $2,000 for the Malala Foundation.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Link to="/generic" className="image">
+            <img src={pic10} alt="" />
+          </Link>
+          <div className="content">
+            <div className="inner">
+              <header className="major">
+                <h3>Memoirs in Greece</h3>
+              </header>
+              <p>
+                <i>Memoirs</i> is about family memory and the ways our heritage
+                inherently guides our lives. It features two composers including
+                the premiere of Garry Novikoff's original piano piece, Snow
+                Falling For Karen. Select students were given the opportunity to
+                perform in Europe when The International Dance Council,
+                Acclaimed by UNESCO, invited The Coleman School of Dance to
+                debut <i>Memoirs</i> at the 2018 Anapale Dance Festival in
+                Athens, Greece.
+              </p>
+            </div>
+          </div>
+        </section>
+      </section>
+    </div>
+  </Layout>
 )
 
 export default Landing
