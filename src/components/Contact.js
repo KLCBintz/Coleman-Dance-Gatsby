b241e33db57e7d0d9acd669d474a5df90040b752
KLCBintz <katie@swirlwebdesign.com>
2020-04-08 15:07:02 -0500

    made form functional

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,18 @@ const Contact = props => (
   <section id="contact">
     <div className="inner">
       <section>
-        <form method="post" action="#">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </p>
           <div className="field half first">
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" />
